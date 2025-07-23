5ee31c615483f4222bc092a35c3243dd06b4b16b
Skorejen <m.ciebien12@gmail.com>
2025-07-23 13:28:10 +0200

    Initial commit

---
+++ ConcurrencyProject/Program.cs
@@ -0,0 +1,83 @@
+﻿
+
+
+
+using System.Collections.Concurrent;
+
+public class Counter
+{
+    public static BlockingCollection<int> totalValues = new();
+    private static bool _IsFinished = false;
+
+    static void Main(string[] args)
+    {
+        Thread t1 = new(() => Counter.InsertOne());
+        t1.Name = "Thread 1";
+
+        Thread t2 = new(() => Counter.TakeOne());
+        t2.Name = "Thread 2";
+
+        Thread t3 = new(() =>
+        {
+            for (var i = 0; ; i++)
+            {
+                System.Console.WriteLine("Total values count: " + totalValues.Count);
+                Thread.Sleep(5000);
+                if (!t2.IsAlive) break;
+            }
+        });
+
+        t1.Start();
+        t2.Start();
+        t3.Start();
+
+        t1.Join();
+        t2.Join();
+        t3.Join();
+        
+        foreach (var item in totalValues)
+        {
+            System.Console.WriteLine(item + "'\n");
+        }
+
+
+
+
+    }
+
+    public delegate void CountTo20();
+    public static void InsertOne()
+    {
+        for (var a = 0; a < 50; a++)
+        {
+            var i = new Random().Next();
+            totalValues.Add(i);
+            System.Console.WriteLine(Thread.CurrentThread.Name + " added value " + i);
+            Thread.Sleep(50);
+        }
+        totalValues.CompleteAdding();
+        
+    }
+    public static void TakeOne()
+    {
+
+        foreach (var item in totalValues.GetConsumingEnumerable())
+        {
+            System.Console.WriteLine($"Thread {Thread.CurrentThread.Name} took value {item}");
+            Thread.Sleep(200);
+        }
+    }
+
+}
+
+public class Producer
+{
+
+
+
+}
+
+public class Receiver
+{
+
+}
---
+++ MyConcurrencyProject/Program.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections.Concurrent;
+using System.Runtime.ExceptionServices;
+
+
+namespace Concurrency
+{
+    class MainClass
+    {
+
+        static void Main(string[] args)
+        {
+
+            List<MyTask> tasks = new();
+            for (int i = 0; i < 1000; i++)
+            {
+                int capturedValue = i;
+                tasks.Add(MyTask.Run(() =>
+                {
+                    System.Console.WriteLine(capturedValue);
+                    Thread.Sleep(1000);
+                }));
+            }
+            Console.ReadLine();
+        }
+    }
+    class MyTask
+    {
+        private bool _completed;
+        private Exception? _exception;
+        private Action? _continuation;
+        private ExecutionContext? _context;
+        public bool IsCompleted
+        {
+            get
+            {
+                lock (this)
+                {
+                    return _completed;
+                }
+
+            }
+        }
+
+        public void SetResult() => Complete(null);
+        public void SetException(Exception exception) => Complete(null);
+
+        private void Complete(Exception? exception)
+        {
+            lock (this)
+            {
+                if (_completed) throw new InvalidOperationException("Stop messing up my code");
+
+                _completed = true;
+                _exception = exception;
+
+                if (_continuation is not null)
+                {
+                    MyThreadPool.QueueUserWorkItem(delegate
+                    {
+                        if (_context is null)
+                        {
+                            _continuation();
+                        }
+                        else
+                        {
+                            ExecutionContext.Run(_context, (object? state) => ((Action)state!).Invoke(), _continuation);
+                        }
+                    });
+                }
+            }
+
+        }
+        public void Wait()
+        {
+            ManualResetEventSlim? mres = null;
+
+            lock (this)
+            {
+                if (!_completed)
+                {
+                    mres = new ManualResetEventSlim();
+                    ContinueWith(mres.Set);
+                }
+            }
+
+            mres?.Wait();
+
+            if (_exception is not null)
+            {
+                ExceptionDispatchInfo.Throw(_exception);
+            }
+        }
+        public void ContinueWith(Action action)
+        {
+            lock (this)
+            {
+                if (_completed)
+                {
+                    MyThreadPool.QueueUserWorkItem(action);
+                }
+                else
+                {
+                    _continuation = action;
+                    _context = ExecutionContext.Capture();
+                }
+            }
+        }
+
+        public static MyTask Run(Action action)
+        {
+            MyTask t = new();
+            MyThreadPool.QueueUserWorkItem(() =>
+            {
+                try
+                {
+                    action();
+                }
+                catch (Exception e)
+                {
+                    t.SetException(e);
+                    return;
+                }
+                t.SetResult();
+            });
+            return t;
+        }
+    }
+
+
+
+    static class MyThreadPool
+    {
+        private static readonly BlockingCollection<(Action, ExecutionContext?)> s_workItems = new();
+
+        public static void QueueUserWorkItem(Action action) => s_workItems.Add((action, ExecutionContext.Capture()));
+
+
+
+        static MyThreadPool()
+        {
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+            {
+                new Thread(() =>
+                {
+                    while (true)
+                    {
+                        (Action workItem, ExecutionContext? context) = s_workItems.Take();
+                        if (context is null)
+                        {
+                            workItem();
+                        }
+                        else
+                        {
+                            ExecutionContext.Run(context, delegate { workItem(); }, null);
+
+                            ExecutionContext.Run(context, state => ((Action)state!).Invoke(), workItem);
+                        }
+                        workItem();
+                    }
+                })
+                {
+                    IsBackground = true
+                }.Start();
+            }
+        }
+    }
+}
